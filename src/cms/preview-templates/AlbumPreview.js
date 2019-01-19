ed79a76a61ea9db11f3f7519a2551945935ae0e3
Larry Hudson <harryludson@gmail.com>
2019-01-19 16:19:04 +1100

    tracking files that need to be tracked :-)

---
+++ src/cms/preview-templates/AlbumPreview.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AlbumTemplate } from '../../templates/album'
+
+const AlbumPreview = ({ entry, widgetFor }) => (
+  <AlbumTemplate
+    content={widgetFor('body')}
+    blurb={entry.getIn(['data', 'blurb'])}
+    tags={entry.getIn(['data', 'tags'])}
+    title={entry.getIn(['data', 'title'])}
+    image={entry.getIn(['data', 'image'])}
+  />
+)
+
+AlbumTemplate.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default AlbumPreview
---
+++ src/components/AlbumCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import { navigate } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+const AlbumCard = ({ album }) => (
+	<div
+		style={{
+			cursor: 'pointer',
+			border: '1px black solid',
+			borderRadius: '0.5em',
+			padding: '1em'}}
+		key={album.id}
+		onClick={() => navigate(album.fields.slug)}
+	>
+		<div>
+			<PreviewCompatibleImage imageInfo={album.frontmatter.image} />
+		</div>
+		<div>
+			<h2><Link className="has-text-primary" to={album.fields.slug}>{album.frontmatter.title}</Link></h2>
+			<p>{album.frontmatter.artist}</p>
+			<p><small>{album.frontmatter.blurb}</small></p>
+		</div>
+	</div>
+	)
+
+export default AlbumCard
